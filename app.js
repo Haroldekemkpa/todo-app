2c509fa878ac37106f7017e8c0257392784263e9
HaroldEkemkpa <haroldonyebuchi507@gmail.com>
2023-10-17 22:50:04 +0100

    Javascript first commit

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-// alert("hello welcome")
+window.addEventListener('load', () => {
+    const form = document.querySelector('#new-task-form');
+    const input = document.querySelector('#new-task-input');
+    const listEL = document.querySelector('#task')
+})
